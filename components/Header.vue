99122f89b1fbc402828eca8c9a1acbb4294fc1b4
chinyishan <star138020@gmail.com>
2024-05-07 22:05:38 +0800

    調整 header 和 footer

--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,17 @@
 <template>
-  <header class="main-header">
-    <h1 class="header-title">喵立翰Miao Li-Han，喵的未來，人的希望</h1>
-    <nav class="nav-web">
-      <h2>Miao</h2>
-      <div class="nav-group">
-        <ul class="nav-list">
-          <li><NuxtLink to="/">候選人主張</NuxtLink></li>
-          <li><NuxtLink to="/">最新活動</NuxtLink></li>
-          <li><NuxtLink to="/">政策議題</NuxtLink></li>
-          <li><NuxtLink to="/">小額捐款</NuxtLink></li>
-          <li><NuxtLink to="/">服務信箱</NuxtLink></li>
+  <header class="header">
+    <h1 class="header__title">喵立翰Miao Li-Han，喵的未來，人的希望</h1>
+    <nav class="header__nav">
+      <h2 class="header__logo">Miao</h2>
+      <div class="header__group">
+        <ul class="header__list">
+          <li class="header__item"><NuxtLink to="/">候選人主張</NuxtLink></li>
+          <li class="header__item"><NuxtLink to="/">最新活動</NuxtLink></li>
+          <li class="header__item"><NuxtLink to="/">政策議題</NuxtLink></li>
+          <li class="header__item"><NuxtLink to="/">小額捐款</NuxtLink></li>
+          <li class="header__item"><NuxtLink to="/">服務信箱</NuxtLink></li>
         </ul>
-        <div class="nav-link">
+        <div class="header__links">
           <a href="#" target="_blank"
             ><img
               src="@/assets/images/ic-fb.png"
@@ -29,6 +29,20 @@
           /></a>
         </div>
       </div>
+      <div
+        class="header__hamburger"
+        @click="isOpenClose"
+        :class="{ openClose: openClose }"
+      >
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+      </div>
     </nav>
   </header>
 </template>
+
+<script scope>
+const isOpenClose = ref(false);
+const openClose = ref(false);
+</script>
